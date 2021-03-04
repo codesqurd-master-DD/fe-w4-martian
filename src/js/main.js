d86d5d4d135b7278d5a8702befb083ba614e83ef
jjunyjjuny <jjuny546@gmail.com>
2021-03-04 18:40:30 +0900

    전송 클릭시 해당 행성으로 5초에 한 글자씩 메세지 전송

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,66 @@
 import "../scss/main.scss";
 import { drawTransceiver } from "./transceiver.js";
+
 window.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
-  const $space = document.getElementById("space");
-  const $earth = document.getElementById("earth");
   const $mars = document.getElementById("mars");
-
-  initPlanet([$earth, $mars]);
-
-  setEvents($space);
+  const $earth = document.getElementById("earth");
+  initPlanet([$mars, $earth]);
+  setEvents();
 }
 function initPlanet(planets) {
   planets.forEach(($planet) => {
     drawTransceiver($planet);
   });
 }
-function setEvents($space) {
+function setEvents() {
+  const $space = document.getElementById("space");
+
   addEvent($space, "input", ".send>input", ({ target }) => {
-    const hex = translateDecToHex(target.value);
-    const $planet = target.closest(".planet");
-    console.log($planet);
+    const hex = target.value.split("").map(translateDecToHex);
+    const $planet = getPlanet(target);
     $planet.querySelector(".convertedBox").innerText = hex.join(" ");
   });
-}
 
-function convertText(value, type) {}
-function translateDecToHex(dec) {
-  return dec.split("").map((el) => el.charCodeAt(0).toString(16));
+  addEvent($space, "click", "[data-to]", ({ target }) => {
+    const message = getSendMessage(getPlanet(target));
+    const $targetPlanet = document.getElementById(`${target.dataset.to}`);
+    console.log(message);
+    sendMessage($targetPlanet, message);
+  });
 }
-function translateHexToDec() {}
+// 여기부터 시작~
+function sendMessage($targetPlanet, message) {
+  if (!message.length) {
+    console.log("done");
+    return;
+  }
+  setTimeout(() => {
+    const char = message.shift();
+    receiveMessage($targetPlanet, char);
+    sendMessage($targetPlanet, message);
+  }, 1000);
+}
+function receiveMessage($targetPlanet, char) {
+  const dec = String.fromCharCode(translateHexToDec(char));
+  $targetPlanet.querySelector(".reception input").value += ` ${dec}`;
+}
+function getPlanet(target) {
+  return target.closest(".planet");
+}
+function getSendMessage($planet) {
+  const convertedBox = $planet.querySelector(".convertedBox");
+  const message = convertedBox.innerText;
+  convertedBox.innerText = "";
+  return message.split(" ");
+}
+function translateDecToHex(dec) {
+  return dec.charCodeAt(0).toString(16);
+}
+function translateHexToDec(hex) {
+  return parseInt(hex, 16);
+}
 
 function addEvent($target, eventType, selector, callback) {
   const children = [...$target.querySelectorAll(selector)];

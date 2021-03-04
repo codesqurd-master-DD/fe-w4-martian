69bb2fa046dbf48cec0c0cfee81984c324e8fce5
jjunyjjuny <jjuny546@gmail.com>
2021-03-04 21:40:11 +0900

    Modify 문자 전송 방식 개선 이전에는 문자 전송과 입력을 5초 간격으로 같은 함수에서 함. 결과 자체는 똑같겠지만 화성에서 전송 -> 지구의 수신기 수신 -> 회전 -> 회전 끝나면 해당 문자 출력 의 흐름을 만들었음(아직 모든 기능 구현x) 객체지향 같은 생각?이라서 함수형에 맞는 방식인지 잘 모르겠다..

--- src/js/main.js
+++ src/js/main.js
@@ -16,35 +16,52 @@ function initPlanet(planets) {
 }
 function setEvents() {
   const $space = document.getElementById("space");
+  $space.addEventListener("send", rotateTransceiver);
 
   addEvent($space, "input", ".send>input", ({ target }) => {
     const hex = target.value.split("").map(translateDecToHex);
     const $planet = getPlanet(target);
     $planet.querySelector(".convertedBox").innerText = hex.join(" ");
   });
-
   addEvent($space, "click", "[data-to]", ({ target }) => {
     const message = getSendMessage(getPlanet(target));
     const $targetPlanet = document.getElementById(`${target.dataset.to}`);
-    console.log(message);
-    sendMessage($targetPlanet, message);
+    sendMessage(target, $targetPlanet, message);
+  });
+  addEvent($space, "transitionend", ".camera", ({ target }) => {
+    // camera의 rotate가 끝나면 실행
+    const char = getCharFromDegree(target);
+    const $targetPlanet = target.closest(".planet");
+    receiveMessage($targetPlanet, char);
   });
 }
 // 여기부터 시작~
-function sendMessage($targetPlanet, message) {
+function sendMessage(target, $targetPlanet, message) {
   if (!message.length) {
     console.log("done");
     return;
   }
   setTimeout(() => {
     const char = message.shift();
-    receiveMessage($targetPlanet, char);
-    sendMessage($targetPlanet, message);
+    const sendEvent = new CustomEvent("send", {
+      bubbles: true,
+      detail: {
+        char,
+        $targetPlanet,
+      },
+    });
+    target.dispatchEvent(sendEvent);
+    // receiveMessage($targetPlanet, char);
+    sendMessage(target, $targetPlanet, message);
   }, 1000);
+}
+function rotateTransceiver({ detail: { char, $targetPlanet } }) {
+  const camera = $targetPlanet.querySelector(".camera");
+  // camera rotate
 }
 function receiveMessage($targetPlanet, char) {
   const dec = String.fromCharCode(translateHexToDec(char));
-  $targetPlanet.querySelector(".reception input").value += ` ${dec}`;
+  $targetPlanet.querySelector(".reception input").value += dec;
 }
 function getPlanet(target) {
   return target.closest(".planet");
--- src/js/transceiver.js
+++ src/js/transceiver.js
@@ -1,10 +1,10 @@
 export function drawTransceiver($planet) {
-  const $target = $planet.querySelector(".transceiver");
+  const $target = $planet.querySelector(".transceiverZone");
   $target.innerHTML = _createParts();
 }
 
 function _createParts() {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const numbers = [...Array(10), "A", "B", "C", "D", "E", "F"];
   return numbers
     .map((num, i) => {
       return `<div class="part" style="transform:rotate(${
